558e0d4725f2b39baffb90979c21628970249b18
KcNcoG <86236634+KcNcoG@users.noreply.github.com>
2021-06-24 22:29:01 +0800

    Update insertion_sort.py

--- Sort Algorithms/Annotated Version/insertion_sort.py
+++ Sort Algorithms/Annotated Version/insertion_sort.py
@@ -1,5 +1,7 @@
 # insertion_sort.py
 
+# 对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+
 # def insertion_sort(arr):
 #     length = len(arr)
 #     for i in range(1, length):
@@ -9,12 +11,19 @@
 
 def insertion_sort(arr):
     length = len(arr)
+    # 外层循环控制遍历的次数，也是该次遍历进行插入的元素的索引
+    # 插入排序中i的取值范围为[1,length-1]
+    # 1 第一个元素可认为已排序，从第二个元素开始插入
     for i in range(1, length):
+        # key保存当前插入元素的值
         key = arr[i]
+        # 内层循环将该次遍历要插入的元素arr[i]插入到指定位置
+        # 由于比较时是arr[j]与arr[j+1]比较，因此j的取值范围为[0,i-1]
         j = i - 1
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j = j - 1
+        # 找到适合插入的位置，进行插入
         arr[j + 1] = key
 
 
