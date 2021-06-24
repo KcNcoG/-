c5055f02ab14b14445ddaee7acfb64feca6e95fb
KcNcoG <86236634+KcNcoG@users.noreply.github.com>
2021-06-24 21:14:32 +0800

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,17 +2,17 @@
 
 def bubble_sort(arr):
     length = len(arr)
-    for i in range(length - 1, 0, -1):  # 遍历未排序部分数组
-        exchange = False
-        for j in range(i):
+    for i in range(length - 1):
+        swapped = False
+        for j in range(0, length - 1 - i):
             if arr[j] > arr[j + 1]:
-                exchange = True
+                swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        if not exchange:
+        if not swapped:
             break
 
 
 if __name__ == '__main__':
-    test = [2, 8, 2, 7, 1, 5, 6, 4, 7, 9]
+    test = [8, 62, 82, 28, 70, 55, 60, 49, 66, 65]
     bubble_sort(test)
     print(test)
